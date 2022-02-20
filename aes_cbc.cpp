be58126a769431982155e4230f59f92ab3d8a43a
nk125 <nekeselgamer@gmail.com>
2022-02-19 19:44:43 -0600

    Include file and examples

---
+++ aes_cbc.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "binaryhandling.hpp"
+#include "plusaes_wrapper.hpp"
+
+int main(int argc, char* argv[]) {
+    std::string type, in, out, pass, con, aes_str, prefix;
+    bool decrypt_mode;
+    int pass_sz;
+    nk125::plusaes_wrapper aes;
+    nk125::binary_file_handler b;
+
+    if (argc <= 1) {
+        std::cerr << "Usage: -d/-e [Input File] [Output File] [Password]\n";
+        return 1;
+    }
+    if (argc <= 2) {
+        std::cerr << "Input file isn't defined!\n";
+        return 1;
+    }
+    else if (argc <= 3) {
+        std::cerr << "Output file isn't defined!\n";
+        return 1;
+    }
+    else if (argc <= 4) {
+        std::cerr << "Password isn't defined!\n";
+        return 1;
+    }
+    else {
+        type.assign(argv[1]);
+        in.assign(argv[2]);
+        out.assign(argv[3]);
+        pass.assign(argv[4]);
+        pass_sz = pass.size();
+        decrypt_mode = type == "-d";
+        if (!decrypt_mode && type != "-e") {
+            std::cerr << "Unknown option: " << type << "\n";
+            return 1;
+        }
+
+        prefix = (decrypt_mode ? "De" : "En");
+
+        switch (pass_sz) {
+            case 16:
+                aes_str = "AES-128";
+                break;
+            case 24:
+                aes_str = "AES-192";
+                break;
+            case 32:
+                aes_str = "AES-256";
+                break;
+            default:
+                std::cerr << "You need a password with 16, 24 or 32 bytes/chars size!\n";
+                return 1;
+                break;
+        }
+
+        pass_sz = 0;
+
+        try {
+            con = b.read_file(in);
+            //std::string iv = "nk125&pa_endelib";
+            // NK125 and PlusAES Encrypt-Decrypt Library (for C++)
+            // Only for use in ECB En-Decryption AES type
+            //aes.set_iv(reinterpret_cast<unsigned char*>(iv.data()));
+
+            if (!decrypt_mode) {
+                con = aes.cbc_encrypt(con, pass);
+            }
+            else {
+                con = aes.cbc_decrypt(con, pass);
+            }
+
+            pass = "";
+            // Maximize the security deleting the size and undefining password after use
+            if (con.empty()) {
+                std::cerr << "Error at " << prefix << "crypting\n";
+                return 1;
+            }
+
+            b.write_file(out, con);
+        }
+        catch (std::exception& e) {
+            std::cerr << "Exception thrown: " << e.what() << "\n";
+            return 2;
+        }
+    }
+
+    std::cout << "Finished!\n" << prefix << "crypted file succesfully with " << aes_str << "\n";
+    return 0;
+}
