4f2ae81c0541fb540888dcc6f2dd8cd53222e7e4
nk125 <nekes125@outlook.com>
2022-05-13 19:57:06 -0500

    C++ <random> header implemented
    Now uniform_int_distribution is used to gen random strings instead of rand(), improving security (not cryptographically secure, but more secure than rand) and more updated to C++ standard

--- include/plusaes_wrapper.hpp
+++ include/plusaes_wrapper.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <chrono>
 #include "plusaes.hpp"
+#include <random>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -13,20 +13,19 @@ namespace nk125 {
                 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
             };
 
-            unsigned char m_tw_iv[12] = {
+            unsigned char m_nonce[12] = {
                 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
                 0x08, 0x09, 0x0A, 0x0B
             };
 
-            unsigned char m_tag[16];
+            unsigned char m_tag[16] = {0};
 
             bool copy_str(std::vector<unsigned char>* v, std::string str) {
                 std::vector<unsigned char> buffer;
                 // I need a buffer if vector v is undefined/cleared/modified under execution
                 if (str.size() == 0) return 0;
-                for (unsigned char c : str) {
-                    buffer.push_back(c);
-                }
+                buffer.assign(str.begin(), str.end());
+
                 *v = buffer;
                 return 1;
             }
@@ -40,19 +39,6 @@ namespace nk125 {
                 return buffer;
             }
             */
-
-            void make_seed() {
-                std::chrono::time_point<std::chrono::high_resolution_clock> cptr = std::chrono::high_resolution_clock::now();
-                srand(std::chrono::duration_cast<std::chrono::nanoseconds> (cptr.time_since_epoch()).count());
-            }
-
-            std::string select_next_char() {
-                std::string dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                int dict_sz = dictionary.size();
-                std::string random_ch = std::string{dictionary[rand() % dict_sz]};
-                return random_ch;
-            }
-
         // End Of Private
 
         public:
@@ -60,8 +46,8 @@ namespace nk125 {
                 strcpy((char*) m_iv, (const char*) iv);
             }
 
-            void set_tw_iv(unsigned char* iv) {
-                strcpy((char*) m_tw_iv, (const char*) iv);
+            void set_nonce(unsigned char* iv) {
+                strcpy((char*) m_nonce, (const char*) iv);
             }
 
             std::string get_tag() {
@@ -76,12 +62,15 @@ namespace nk125 {
 
             std::string gen_random(long long size) {
                 if (size <= 0) return "";
-                std::string random;
+                std::random_device rd;
+                std::mt19937 mt(rd());
 
-                make_seed();
+                std::string random;
+                std::string dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                std::uniform_int_distribution<> randomint(0, dictionary.size() - 1);
 
                 for (long long i = 0; i < size; i++) {
-                    random.append(select_next_char());
+                    random += dictionary.at(randomint(mt));
                 }
 
                 return random;
@@ -93,11 +82,11 @@ namespace nk125 {
 
                 std::string encrypted_buffer;
                 const unsigned long raw_sz = plusaes::get_padded_encrypted_size(raw.size());
-                const unsigned char* passkey = reinterpret_cast<unsigned char*>(pass.data());
+                const unsigned char* passkey = (unsigned char*)(pass.data());
 
                 std::vector<unsigned char> e_raw_data(raw_sz);
 
-                auto res = plusaes::encrypt_cbc(reinterpret_cast<unsigned char*>(raw.data()), orig_raw_sz, passkey, pass_sz, &m_iv, &e_raw_data[0], e_raw_data.size(), true);
+                auto res = plusaes::encrypt_cbc((unsigned char*)(raw.data()), orig_raw_sz, passkey, pass_sz, &m_iv, &e_raw_data[0], e_raw_data.size(), true);
                 if (res) {
                     return "";
                 }
@@ -113,7 +102,7 @@ namespace nk125 {
 
                 std::string decrypted_buffer;
                 unsigned long raw_sz = plusaes::get_padded_encrypted_size(raw.size());
-                const unsigned char* passkey = reinterpret_cast<unsigned char*>(pass.data());
+                const unsigned char* passkey = (unsigned char*)(pass.data());
 
                 std::vector<unsigned char> e_raw_data;
                 if (!copy_str(&e_raw_data, raw)) return "";
@@ -135,11 +124,11 @@ namespace nk125 {
 
                 std::string encrypted_buffer;
                 const unsigned long raw_sz = plusaes::get_padded_encrypted_size(raw.size());
-                const unsigned char* passkey = reinterpret_cast<unsigned char*>(pass.data());
+                const unsigned char* passkey = (unsigned char*)(pass.data());
 
                 std::vector<unsigned char> e_raw_data(raw_sz);
 
-                auto res = plusaes::encrypt_ecb(reinterpret_cast<unsigned char*>(raw.data()), orig_raw_sz, passkey, pass_sz, &e_raw_data[0], e_raw_data.size(), true);
+                auto res = plusaes::encrypt_ecb((unsigned char*)(raw.data()), orig_raw_sz, passkey, pass_sz, &e_raw_data[0], e_raw_data.size(), true);
                 if (res) {
                     return "";
                 }
@@ -155,7 +144,7 @@ namespace nk125 {
 
                 std::string decrypted_buffer;
                 unsigned long raw_sz = plusaes::get_padded_encrypted_size(raw.size());
-                const unsigned char* passkey = reinterpret_cast<unsigned char*>(pass.data());
+                const unsigned char* passkey = (unsigned char*)(pass.data());
 
                 std::vector<unsigned char> e_raw_data;
                 if (!copy_str(&e_raw_data, raw)) return "";
@@ -178,14 +167,14 @@ namespace nk125 {
 
                 std::string encrypted_buffer;
                 const unsigned long raw_sz = plusaes::get_padded_encrypted_size(raw.size());
-                const unsigned char* passkey = reinterpret_cast<unsigned char*>(pass.data());
-                unsigned char* aadata = reinterpret_cast<unsigned char*>(aad.data());
+                const unsigned char* passkey = (unsigned char*)(pass.data());
+                unsigned char* aadata = (unsigned char*)(aad.data());
                 int aadata_sz = aad.size();
 
                 //std::vector<unsigned char> vec_iv(std::begin(m_iv), std::end(m_iv));
                 std::vector<unsigned char> e_raw_data(raw_sz);
 
-                auto res = plusaes::encrypt_gcm(reinterpret_cast<unsigned char*>(raw.data()), orig_raw_sz, aadata, aadata_sz, passkey, pass_sz, &m_tw_iv, &m_tag);
+                auto res = plusaes::encrypt_gcm((unsigned char*)(raw.data()), orig_raw_sz, aadata, aadata_sz, passkey, pass_sz, &m_nonce, &m_tag);
                 if (res) {
                     return "";
                 }
@@ -201,8 +190,8 @@ namespace nk125 {
 
                 std::string decrypted_buffer;
                 unsigned long raw_sz = plusaes::get_padded_encrypted_size(raw.size());
-                const unsigned char* passkey = reinterpret_cast<unsigned char*>(pass.data());
-                unsigned char* aadata = reinterpret_cast<unsigned char*>(aad.data());
+                const unsigned char* passkey = (unsigned char*)(pass.data());
+                unsigned char* aadata = (unsigned char*)(aad.data());
                 int aadata_sz = aad.size();
 
                 // std::vector<unsigned char> vec_iv(std::begin(m_iv), std::end(m_iv));
@@ -214,7 +203,7 @@ namespace nk125 {
                     m_tag[i] = tag[i];
                 }
 
-                auto res = plusaes::decrypt_gcm(&ed_raw_data[0], orig_raw_sz, aadata, aadata_sz, passkey, pass_sz, &m_tw_iv, &m_tag);
+                auto res = plusaes::decrypt_gcm(&ed_raw_data[0], orig_raw_sz, aadata, aadata_sz, passkey, pass_sz, &m_nonce, &m_tag);
                 if (res) {
                     return "";
                 }
